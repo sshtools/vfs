fe3a4654edba7a8d6286007170840e2e0095ec11
ludup <lee@ludup.com>
2018-12-28 13:32:44 +0000

    Upgrade to latest google api to fix HSF dependency conflict. Tika dependency appears to be no longer available so refactored to let google decide mime type.

--- vfs-googledrive/src/main/java/com/sshtools/vfs/googledrive/GDriveFileSystem.java
+++ vfs-googledrive/src/main/java/com/sshtools/vfs/googledrive/GDriveFileSystem.java
@@ -8,7 +8,7 @@ import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileSystemOptions;
 import org.apache.commons.vfs2.provider.AbstractFileName;
 import org.apache.commons.vfs2.provider.AbstractFileSystem;
-import org.apache.tika.Tika;
+//import org.apache.tika.Tika;
 
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -18,19 +18,19 @@ public class GDriveFileSystem extends AbstractFileSystem {
 
 	private Drive drive;
 	private NetHttpTransport httpTransport;
-	private Tika tika;
+//	private TiCka tika;
 
 	public GDriveFileSystem(GDriveFileName fileName, Drive drive, FileSystemOptions fileSystemOptions,
 			NetHttpTransport httpTransport) throws FileSystemException {
 		super(fileName, null, fileSystemOptions);
 		this.drive = drive;
 		this.httpTransport = httpTransport;
-		tika = new Tika();
+//		tika = new Tika();
 	}
 
-	public Tika getTika() {
-		return tika;
-	}
+//	public Tika getTika() {
+//		return tika;
+//	}
 
 	@Override
 	protected void addCapabilities(Collection<Capability> capabilities) {
