f7c7c2541f70ff526e1c8f218a8b7fa5d3a7009f
Boris Petrov <boris_petrov@live.com>
2020-12-14 11:54:18 +0200

    Fix Azure file names including `azure`

--- vfs-azure/src/main/java/com/sshtools/vfs/azure/AzureFileName.java
+++ vfs-azure/src/main/java/com/sshtools/vfs/azure/AzureFileName.java
@@ -2,17 +2,13 @@ package com.sshtools.vfs.azure;
 
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileType;
-import org.apache.commons.vfs2.provider.GenericFileName;
+import org.apache.commons.vfs2.provider.AbstractFileName;
 import org.apache.commons.vfs2.provider.UriParser;
 
-public class AzureFileName extends GenericFileName {
+public class AzureFileName extends AbstractFileName {
 
-	protected AzureFileName(final String path, final FileType type) {
-		this(null, null, path, type);
-	}
-
-	protected AzureFileName(String userName, String password, final String path, final FileType type) {
-		super("azure", "azure", 443, 443, userName, password, path, type);
+	protected AzureFileName(final String scheme, final String path, final FileType type) {
+		super(scheme, path, type);
 	}
 
 	public String getContainer() {
@@ -28,7 +24,12 @@ public class AzureFileName extends GenericFileName {
 
 	@Override
 	public FileName createName(String absPath, FileType type) {
-		return new AzureFileName(getUserName(), getPassword(), absPath, type);
+		return new AzureFileName(getScheme(), absPath, type);
 	}
 
+	@Override
+	protected void appendRootUri(StringBuilder buffer, boolean addPassword) {
+		buffer.append(getScheme());
+		buffer.append("://");
+	}
 }
