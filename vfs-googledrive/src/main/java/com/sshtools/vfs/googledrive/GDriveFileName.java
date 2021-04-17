28f3c04d23efb17e95507e53edf3aa6631792708
Lee Painter <lee@ludup.com>
2021-04-17 16:31:45 +0100

    Revert "Remove unused methods"
    This reverts commit ed2dd15e8b6c0c7ee7bf8eca99a3d708c631a621.

--- vfs-googledrive/src/main/java/com/sshtools/vfs/googledrive/GDriveFileName.java
+++ vfs-googledrive/src/main/java/com/sshtools/vfs/googledrive/GDriveFileName.java
@@ -3,11 +3,23 @@ package com.sshtools.vfs.googledrive;
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.provider.AbstractFileName;
+import org.apache.commons.vfs2.provider.UriParser;
 
 public class GDriveFileName extends AbstractFileName {
 
 	protected GDriveFileName(String scheme, String path, FileType type) {
 		super(scheme, path, type);
+	}
+
+	public String getContainer() {
+		StringBuilder bui = new StringBuilder(getPath());
+		return UriParser.extractFirstElement(bui);
+	}
+
+	public String getPathAfterContainer() {
+		StringBuilder bui = new StringBuilder(getPath());
+		UriParser.extractFirstElement(bui);
+		return bui.toString();
 	}
 
 	@Override
