fc7bb8415ac99839cbd13fd6ecae3057f04b3223
Lee Painter <lee@ludup.com>
2021-04-17 16:32:07 +0100

    Revert "Remove unused methods"
    This reverts commit a2d77bce0f1c29e953c6c95e2f544d0ee5b29585.

--- vfs-azure/src/main/java/com/sshtools/vfs/azure/AzureFileName.java
+++ vfs-azure/src/main/java/com/sshtools/vfs/azure/AzureFileName.java
@@ -3,11 +3,23 @@ package com.sshtools.vfs.azure;
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.provider.AbstractFileName;
+import org.apache.commons.vfs2.provider.UriParser;
 
 public class AzureFileName extends AbstractFileName {
 
 	protected AzureFileName(final String scheme, final String path, final FileType type) {
 		super(scheme, path, type);
+	}
+
+	public String getContainer() {
+		StringBuilder bui = new StringBuilder(getPath());
+		return UriParser.extractFirstElement(bui);
+	}
+
+	public String getPathAfterContainer() {
+		StringBuilder bui = new StringBuilder(getPath());
+		UriParser.extractFirstElement(bui);
+		return bui.toString();
 	}
 
 	@Override
