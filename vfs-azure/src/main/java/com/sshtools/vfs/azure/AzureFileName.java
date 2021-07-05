5413dcbe5fab99c7887172e1e1d3efc56772f19a
Lee Painter <lee@ludup.com>
2021-07-05 20:17:04 +0100

    Appears there were missing methods on AzureFileName.

--- vfs-azure/src/main/java/com/sshtools/vfs/azure/AzureFileName.java
+++ vfs-azure/src/main/java/com/sshtools/vfs/azure/AzureFileName.java
@@ -3,6 +3,7 @@ package com.sshtools.vfs.azure;
 import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.provider.AbstractFileName;
+import org.apache.commons.vfs2.provider.UriParser;
 
 public class AzureFileName extends AbstractFileName {
 
@@ -19,5 +20,16 @@ public class AzureFileName extends AbstractFileName {
 	protected void appendRootUri(StringBuilder buffer, boolean addPassword) {
 		buffer.append(getScheme());
 		buffer.append("://");
+	}
+	
+	public String getContainer() {
+		StringBuilder bui = new StringBuilder(getPath());
+		return UriParser.extractFirstElement(bui);
+	}
+
+	public String getPathAfterContainer() {
+		StringBuilder bui = new StringBuilder(getPath());
+		UriParser.extractFirstElement(bui);
+		return bui.toString();
 	}
 }
