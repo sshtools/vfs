7017c738195ca7ad065edcb814fd7714e5cfe778
ludup <lee@javassh.com>
2022-07-31 17:10:11 +0100

    Supress warning of deprecation

--- vfs-s3/src/main/java/com/sshtools/vfs/s3/provider/s3/S3FileObject.java
+++ vfs-s3/src/main/java/com/sshtools/vfs/s3/provider/s3/S3FileObject.java
@@ -30,7 +30,6 @@ import org.apache.commons.vfs2.FileName;
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileType;
-import org.apache.commons.vfs2.FileUtil;
 import org.apache.commons.vfs2.provider.AbstractFileName;
 import org.apache.commons.vfs2.provider.AbstractFileObject;
 
