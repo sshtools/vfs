533fd948fe345ce9206f25e149547ca940b825f0
Brett Smith <brett@hypersocket.com>
2022-09-16 14:49:02 +0100

    Need newer maven compiler plugin.

--- jafp/src/main/java/module-info.java
+++ jafp/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.sshtools.jafp {
-	requires javax.jmdns;
-	requires org.slf4j;
+	requires transitive javax.jmdns;
+	requires transitive org.slf4j;
 	exports com.sshtools.jafp.client;
 	exports com.sshtools.jafp.common;
 	exports com.sshtools.jafp.server;
